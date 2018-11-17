542f2866855a1235c666e36c4187e211af5d438c
Pdbz199 <279pdbz199@gmail.com>
2018-11-17 02:00:44 -0500

    implemented username checker in bot

--- bot.js
+++ bot.js
@@ -1,4 +1,7 @@
-var tmi = require("tmi.js");
+var tmi = require("tmi.js")
+var isBotUsername = require("./checkUsername.js")
+var repeatMessageSenders = []
+var repeatMessages = new Map()
 
 //BOT CONFIG
 var options = {
@@ -13,16 +16,24 @@ var options = {
 		password : "oauth:j4mtn62qgo0az6d9yrnzwe8swhc2m3"
 	},
 	channels : ["#pdbz199"]
-};
+}
 
-var bot = new tmi.client(options);
+var bot = new tmi.client(options)
 
-bot.connect();
+bot.connect()
 
 bot.on("connected", function(address, port) {
-    bot.say("#pdbz199", "The bot has been activated!");
-});
+    bot.say("#pdbz199", "The bot has been activated!")
+})
 
 bot.on("chat", function(channel, sender, message, self) {
-	if (self) return;
+	if (self) return
+
+	if(isBotUsername(sender.username) && !(sender.mod || sender.username === channel.substring(1))) {
+		for (let i = 0; i < repeatMessageSenders.length; i++) {
+			client.ban(channel, repeatMessageSenders[i], "Spam bot")
+		}
+	}
 })
+
+// let usernames = [/*first three are actual bot names*/ "noisyairfields539", "isolatedfeeds76", "frightenedounce7", "pdbz199"]
--- checkUsername.js
+++ checkUsername.js
@@ -1,35 +1,38 @@
 var checkWord = require('check-word')
 var words = checkWord('en')
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-var numRE = new RegExp(numbers.join("|"))
-var hasNums = new Map()
 
 /**
  * @param {string} username
- * @param {RegExp} regExp
  */
-var usernameSplitter = (username, regExp) => {
+var isBotUsername = (username) => {
+    let splitUsername = usernameSplitter(username)
+
+    if (typeof splitUsername == "object" && isTwoWords(splitUsername[0])) return true
+    return false
+}
+
+/**
+ * @param {string} username
+ */
+var usernameSplitter = (username) => {
+    let regExp = new RegExp(numbers.join("|"))
     let splitUsername = username.split('')
 
     let match = regExp.exec(username)
     if (match) {
-        hasNums.set(username, true)
-
         username = username.replace(match[0], '<?>' + match[0])
-
-        let numNums = username.length - match.index
-        let splitter = `${match[0]}`
-
-        for (let j = match.index + 1; j < username.length; j++) {
-            splitter += splitUsername[j]
-        }
-
         splitUsername = username.split('<?>')
-
+        
         return splitUsername
+    } else {
+        return false
     }
 }
 
+/**
+ * @param {string} username
+ */
 var isTwoWords = (username) => {
     for (let i = 2; i < username.length; i++) {
         let firstWord = username.substring(0, i)
@@ -42,13 +45,4 @@ var isTwoWords = (username) => {
     return false
 }
 
-var isBotUsername = (username) => {
-    let splitUsername = usernameSplitter(username, numRE)
-
-    if (hasNums.get(username) && isTwoWords(splitUsername[0])) return true
-    return false
-}
-
-let usernames = ["noisyairfields539", "isolatedfeeds76", "frightenedounce7", "pdbz199", "metaknight91"]
-
-console.log(isBotUsername(usernames[3]))
+module.exports = isBotUsername;
